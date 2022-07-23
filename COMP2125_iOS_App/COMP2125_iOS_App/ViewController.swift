7f17d2daf7753a439c62329fa77ff2dd9e19ea85
Bindiya Patel <bindiyapatel7069@gmail.com>
2022-07-23 01:08:20 -0700

    initial commit

---
+++ COMP2125_iOS_App/COMP2125_iOS_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  COMP2125_iOS_App
+//
+//  Created by user211287 on 7/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
